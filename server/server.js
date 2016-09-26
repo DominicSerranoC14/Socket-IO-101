2c4366f3da2021082e699fa48689b189e198ee9b
Dominic Serrano <dmsrojo@gmail.com>
2016-09-26 12:31:35 -0500

    Can now add a message from the front end, and update

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const { connect } = require('./db/database');
+const { json } = require('body-parser');
 const port = process.env.PORT || 3000;
 const Message = require('./models/message');
 app.set('port', port);
@@ -13,20 +14,31 @@ app.set('port', port);
 //Middle wares
 //Express.static is based on where 'server.js' is called
 app.use(express.static('client'));
+app.use(json());
 /////////////////////////////////////////
 
 
 /////////////////////////////////////////
 // Routes
 app.get('/api/title', (req, res) => {
-  res.send({title: 'MEAN Chat'});
+  res.json({title: 'MEAN Chat'});
+});
+
+//Find all messages and display from db
+app.get('/api/messages', (req, res, err) => {
+  Message.find()
+  .then((messages) => res.json({messages}))
+  .catch(err)
 });
 
 
-app.get('/api/messages', (req, res, err) => {
-  Message.find()
-    .then((messages) => res.json({messages}))
-    .catch(err)
+//Handle a front-end posted msg
+app.post('/api/messages', (req, res, err) => {
+  const msg = req.body;
+
+  Message.create(msg)
+  .then(msg => res.json(msg))
+  .catch(err);
 });
 /////////////////////////////////////////
 
